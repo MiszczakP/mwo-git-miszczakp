03e95df718ddcbd2fb3b28664ddc4432c97f3896
CodeWithN <gmiterek@student.agh.edu.pl>
2022-01-23 11:41:58 +0100

    new for Rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,7 +3,8 @@ public class SomeProgram {
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
 System.out.println("We will be Git Experts, soon.");
-
+public void manageRockets() {
+    // TODO implement
     }
 }
 
